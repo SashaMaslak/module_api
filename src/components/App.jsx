3441f0056669ef52a5f3f67b8d1ac29965baf403
Sasha Maslak <95244349+SashaMaslak@users.noreply.github.com>
2023-02-09 10:23:44 +0200

    second

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,91 @@
 import { Component } from 'react';
 import { GlobalStyle } from './GlobalStyle';
 import { Layout } from './Layout';
+import { MaterialEditorForm } from './MaterialEditorForm/MaterialEditorForm';
+import { MaterialList } from './MaterialList/MaterialList';
+import * as API from 'services/api';
 
 export class App extends Component {
+  state = {
+    materials: [],
+    isLoading: false,
+    error: null,
+  };
+
+  async componentDidMount() {
+    try {
+      this.setState({ isLoading: true });
+      const materials = await API.getMaterials();
+      this.setState({ materials, isLoading: false });
+    } catch (error) {
+      this.setState({ error: true });
+      console.log(error);
+    }
+  }
+
+  addMaterial = async values => {
+    try {
+      this.setState({ isLoading: true });
+      const material = await API.addMaterial(values);
+      this.setState(state => ({
+        materials: [...state.materials, material],
+        // isLoading: false,
+      }));
+    } catch (error) {
+      console.log(error);
+    } finally {
+      this.setState({
+        isLoading: false,
+      });
+    }
+  };
+
+  deleteMaterial = async id => {
+    try {
+      await API.deleteMaterial(id);
+      this.setState(state => ({
+        materials: state.materials.filter(material => material.id !== id),
+      }));
+    } catch (error) {
+      this.setState({ error: true });
+      console.log(error);
+    }
+  };
+
+  updateMaterial = async fields => {
+    try {
+      const updatedMaterial = await API.updateMaterial(fields);
+      this.setState(state => ({
+        materials: state.materials.map(material =>
+          material.id === fields.id ? updatedMaterial : material
+        ),
+      }));
+    } catch (error) {
+      this.setState({ error: true });
+      console.log(error);
+    }
+  };
+
   render() {
+    const { materials, isLoading, error } = this.state;
     return (
       <Layout>
         <GlobalStyle />
+        <button onClick={this.updateMaterial}>UPDATE MATERIAL</button>
+        {error && <p>!!!Щось пішло не так!!!</p>}
+        <MaterialEditorForm onSubmit={this.addMaterial} />
+        {isLoading ? (
+          'LOADING'
+        ) : (
+          <MaterialList
+            items={materials}
+            onDelete={this.deleteMaterial}
+            onUpdate={this.updateMaterial}
+          />
+        )}
       </Layout>
     );
   }
 }
+
+// https://63e36c2cc919fe386c060d10.mockapi.io/materials
